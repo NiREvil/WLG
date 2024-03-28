3dee64e8dbb51b45d6d4da2d1b57553d6ca1f011
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-03-29 00:41:10 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ echo "cloning repo"
 git clone https://github.com/NiREvil/WLG.git
 cd WLG
 echo "installing requirments"
-chmod +x requirement.txt && requirement.txt
+chmod +x requirement.sh
+./requirement.sh
+python3 dos2unix.py
 echo "requirements installed successfully."
 echo "Now Starting..."
 python3 main.py
