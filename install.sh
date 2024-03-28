0994c842e825fd2b9657d916a4181a47259fb3f3
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-03-28 08:35:30 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ echo "cloning repo"
 git clone https://github.com/NiREvil/warp-license-generator.git
 cd warp-license-generator
 echo "installing requirments"
-chmod +x requirement.txt && bash requirement.txt
+chmod +x requirement.txt
+bash requirement.txt
 echo "requirements installed successfully."
 echo "Installation complete"
 echo "Now Starting..."
