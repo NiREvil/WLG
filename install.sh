0e959042d670bc870fe79f05d9e4c8bdc0d4d3a9
Nescafe <127026836+nescafetor@users.noreply.github.com>
2024-03-29 00:20:05 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "cloning repo"
 git clone https://github.com/NiREvil/WLG.git
 cd WLG
 echo "installing requirments"
-chmod +x requirement.txt && requirement.txt
+chmod +x requirement.txt && bash requirement.txt
 echo "requirements installed successfully."
 echo "Now Starting..."
 python3 main.py
