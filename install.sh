2781252b1173ddcf315d249473b0589f4b81788f
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-03-28 09:23:20 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+#colors
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+white='\033[0;37m'
+rest='\033[0m'
+
 echo "cloning repo"
 git clone https://github.com/NiREvil/warp-license-generator.git
 cd warp-license-generator
@@ -7,6 +18,9 @@ chmod +x requirement.sh
 ./requirement.sh
 python3 dos2unix.py
 echo "requirements installed successfully."
-echo "Installation complete"
 echo "Now Starting..."
+echo "--------------------------------------------"
+echo -e "${red}Developed By:  'Nima radical' ---  github.com/NiREvil  ${rest}"
+echo "--------------------------------------------"
+echo -e "${green}Freedom To Dream${rest}"
 python3 main.py
