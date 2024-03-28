e937a4119acb29556e767c51e1532c8a683907a2
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-03-28 12:08:25 +0330

    Update main.py

--- main.py
+++ main.py
@@ -75,8 +75,8 @@ while a < value_int:
     time.sleep(60)
 
 os.system('cls' if os.name == 'nt' else 'clear')
-print("\033[2;32;40m Below is the generated key list \n")
-print("Each key is usable on up to a maximum of five devices.\n \nplease copy/paste for later use.")
+print("\033[1;36;40mBelow is the generated key list \n")
+print("\033[1;36;40mEach key is usable on up to a maximum of five devices.\n \nplease copy/paste for later use.")
 for x in gkeys:
   print(x)
 
